b1e0bc2859e04fc606c3919c18da1ad8ad855233
zafarshodmonov1996 <zafarshodmonov1996@gmail.com>
2025-03-19 09:10:38 +0500

    z

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 @app.route('/get/<id>', methods=['GET'])
 def get_articles(id):
     return render_template(f'{id}.html')
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
  
 if __name__ == '__main__':
     app.run(debug=True)
